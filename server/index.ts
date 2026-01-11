8f7e8be5661d8d9804d330b55fbd438cca5db008
Radek Burda <radek.burda@konplan.cz>
2026-01-11 10:33:26 +0100

    prepare for vercel deployment

--- server/index.ts
+++ server/index.ts
@@ -1,37 +1,6 @@
-import express, { Request, Response } from 'express';
-import mongoose from 'mongoose';
-import cors from 'cors';
-import dotenv from 'dotenv';
-import authRoutes from './routes/auth';
-import transactionRoutes from './routes/transactions';
+import app from './app';
 
-dotenv.config();
-
-const app = express();
 const PORT = process.env.PORT || 5000;
-
-// Middleware
-app.use(cors());
-app.use(express.json());
-
-// Routes
-app.use('/api/auth', authRoutes);
-app.use('/api/transactions', transactionRoutes);
-
-// MongoDB Connection
-const mongoURI = process.env.MONGODB_URI;
-if (!mongoURI) {
-  console.error('MONGODB_URI is not defined in .env');
-  process.exit(1);
-}
-
-mongoose.connect(mongoURI)
-  .then(() => console.log('Connected to MongoDB'))
-  .catch(err => console.error('MongoDB connection error:', err));
-
-app.get('/', (req: Request, res: Response) => {
-  res.send('My Company Car API is running (TypeScript)');
-});
 
 app.listen(PORT, () => {
   console.log(`Server is running on port ${PORT}`);
